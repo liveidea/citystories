d5a42bebd6ac1d7171c386b5a8c0f9a64c5e9cae
arfelio <arfelio1980@gmail.com>
2014-11-27 23:32:45 +0200

    establish relations from user to story with 'has_and_belongs_to_many'

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,4 @@
 class Story < ActiveRecord::Base
-
+  has_and_belongs_to_many :followers, class_name: "User",
+  	association_foreign_key: "user_id"  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  has_and_belongs_to_many :followed_stories, class_name: "Story",
+  	association_foreign_key: "story_id"
 end
