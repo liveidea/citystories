b0e98e4707710c4cd0efb6e4d2c0fea40dbdc527
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2014-11-29 13:15:30 +0200

    controller stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,16 +1,25 @@
 class StoriesController < ApplicationController
-  
+
   def show
     @story = Story.find_by_id(params[:id])
   end
 
-  def index 
+  def index
     @stories = Story.all
-  end 
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to @story, notice: 'story was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
 
   private
 
   def story_params
-    params.require(:story).permit(:title)
+    params.require(:story).permit(:title, :adress)
   end
 end
