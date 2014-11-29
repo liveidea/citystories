ce532c1f499151f8de47e921eb0fdb435954f30f
arfelio <arfelio1980@gmail.com>
2014-11-29 14:46:51 +0200

    add new and create methods in story_updates controller an view new

--- app/controllers/story_updates_controller.rb
+++ app/controllers/story_updates_controller.rb
@@ -1,2 +1,26 @@
 class StoryUpdatesController < ApplicationController
+  def index
+    @story_updates = StoryUpdate.all
+  end
+
+  def new
+    @story = Story.find_by_id(params[:id])
+    @story_update = StoryUpdate.new
+  end
+
+  def create
+    @story = Story.find_by_id(params[:story_id])
+    @story_update = @story.story_updates.create(story_update_params)
+    if @story_update.save
+      redirect_to story_story_updates_path
+    else
+      render action: "new"
+    end
+  end
+
+  private
+
+  def story_update_params
+    params.require(:story_update).permit(:description,:story_id)
+  end
 end
