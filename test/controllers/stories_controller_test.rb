d44e3784cf2d22655e6245b3fe043620a2b33543
arfelio <arfelio1980@gmail.com>
2014-11-30 14:59:46 +0200

    tests for one model and one controller

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StoriesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:stories)
+  end
 end
