d42920b4c78d6b5207b494d9f235afccf966b9ac
arfelio <arfelio1980@gmail.com>
2014-11-30 15:01:50 +0200

    some fixes after conflict

--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "Story must saves" do
+     story = stories(:one)
+     assert story.save
+   end
 end
