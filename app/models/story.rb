391f4685ee3375d2e39ab3370b4c7cdd10cbdc46
arfelio <arfelio1980@gmail.com>
2014-12-06 14:24:23 +0200

    add destroy edit update actions in story story update

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,7 @@
 class Story < ActiveRecord::Base
   has_and_belongs_to_many :followers, class_name: "User",
   	association_foreign_key: "user_id"
-  has_many :story_updates
+  has_many :story_updates, dependent: :destroy
   belongs_to :user
   validates :title, :adress, presence: true
 end
