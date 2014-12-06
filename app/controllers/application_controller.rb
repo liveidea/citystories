9f1e52af078adbc54df78188a7f0af04f1992a1c
arfelio <arfelio1980@gmail.com>
2014-12-06 17:27:25 +0200

    change pathes after sign in and sign up with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  
+  def after_sign_in_path_for (resource)
+    story_updates_path 
+  end
+  
+  def after_sign_up_path_for (resource)
+    story_updates_path 
+  end
+
   protected
 
   def configure_permitted_parameters
