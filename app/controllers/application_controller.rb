0fbbd024cafab62e3426fa09829587be12d3985f
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2014-12-11 00:51:39 +0200

    some fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
- 
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    p '='*20
+    p params[:locale]
+    p I18n.default_locale
+    I18n.locale = get_locale || I18n.default_locale
   end
+
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
   end
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
 
   def after_sign_in_path_for (resource)
-    profile_path 
+    profile_path
   end
 
   def after_sign_up_path_for (resource)
-    profile_path 
+    profile_path
   end
 
   protected
@@ -25,5 +30,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :nickname
   end
 
+  private
 
+  def get_locale
+    if params[:locale] && params[:locale].size == 2
+      params[:locale]
+    end
+  end
 end
