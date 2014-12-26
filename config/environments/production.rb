a1150315eca34511330e6050dc128e690d8f7ba2
arfelio <arfelio1980@gmail.com>
2014-12-26 21:57:03 +0200

    add ssl to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-
+  config.force_ssl = true
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
